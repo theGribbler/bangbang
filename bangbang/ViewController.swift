7a29547a9e536b2d42fd8b2677bfbf9fe5674adc
Paul Lathrop <paul.lathrop@plathrop-ml1.local>
2017-07-06 20:13:14 -0700

    Initial Commit

---
+++ bangbang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bangbang
+//
+//  Created by Paul Lathrop on 7/6/17.
+//  Copyright © 2017 Paul Lathrop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
